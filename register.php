97b95800b000e04a04af86f3c331332c574e96d1
Johnny-Emad <johnnyjohnnyemnz@gmail.com>
2026-07-20 20:11:16 +0300

    Validation

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php require_once("includes/header.php"); ?>
 
 <form action="./register.php" method="POST">
-    <input type="text" name="username" placeholder="Enter your username" required>
+    <input type="text" name="name" placeholder="Enter your name" required>
     <input type="email" name="email" placeholder="Enter your email" required>
     <input type="tel" name="phone" placeholder="Enter your phone number" required />
     <input type="number" name="age" placeholder="Enter your age" min=5 max=100 required>
@@ -25,5 +25,63 @@
 
     <button type="submit">Register</button>
 </form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    $name = $age = $grade = $score = $gender = $email = $phone = "";
+
+    if (isset($_POST['name']) && strlen($_POST["name"]) >= 3) {
+        $name = htmlspecialchars($_POST['name']);
+    } else {
+        echo "No name given";
+    }
+
+    if (isset($_POST['age']) && filter_var($_POST['age'], FILTER_VALIDATE_INT)) {
+        if ($_POST['age'] >= 5 && $_POST['age'] <= 100) {
+            $age = htmlspecialchars($_POST['age']);
+        } else {
+            $age = "Non valid age given";
+        }
+    } else {
+        echo "No age given";
+    }
+
+    if (isset($_POST['grade'])) {
+        $grade = htmlspecialchars($_POST['grade']);
+    } else {
+        $grade = "No grade given";
+    }
+
+    if (filter_var($_POST['score'], FILTER_VALIDATE_INT) && isset($_POST['score'])) {
+        if ($_POST['score'] >= 0 && $_POST['score'] <= 100) {
+            $score = htmlspecialchars($_POST['score']);
+        } else {
+            $score = "Non valid score given";
+        }
+    } else {
+        $score = "No score given";
+    }
+
+    if (isset($_POST['gender'])) {
+        $gender = htmlspecialchars($_POST['gender']);
+    } else {
+        $gender = "No gender given";
+    }
+
+    if (isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $email = htmlspecialchars($_POST['email']);
+    } else {
+        $email = "No email given";
+    }
+
+    if (isset($_POST['phone']) && strlen($_POST['phone']) == 11) {
+        $phone = htmlspecialchars($_POST['phone']);
+    } else {
+        $phone = "No phone number given";
+    }
+}
+?>
 
 <?php require_once("includes/footer.php");
